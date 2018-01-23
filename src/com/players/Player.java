18f1a7d3cc9a642bd0a2a99500035f57d70192ea
ambrusBence1 <o.g.bence@gmail.com>
2018-01-23 22:25:47 +0100

    new folders

---
+++ src/com/players/Player.java
@@ -0,0 +1,20 @@
+package com.players;
+
+public class Player{
+
+    private String name;
+    private Inventory inventory;
+
+    //constructor
+    public Player(String name){
+        this.name = name;
+    }
+
+    //setters/getters
+
+    public String name(){
+        return name;
+    }
+
+}
+
